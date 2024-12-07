5c3425209a299bb6cfaa5e73ff2a796406801389
HOANGDINHTUNG <mtung3365@gmail.com>
2024-12-07 20:46:16 +0700

    Add files via upload

---
+++ bt5ss2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+	int dai = 5;
+	int rong = 4;
+	int chu_vi = (dai + rong) * 2;
+	int dien_tich = dai * rong;
+	printf("chu_vi= %d\n", chu_vi);
+	printf("dien_tich= %d", dien_tich);
+
+}
